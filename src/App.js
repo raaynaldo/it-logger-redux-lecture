e76f6c266b23729e965a1e5181e29403bf6f8129
raaynaldo <raaynaldo@hotmail.com>
2021-02-13 21:50:27 -0500

    UI until preloader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import Logs from './components/logs/Logs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    useEffect(() => {
+        M.AutoInit();
+    });
+    return (
+        <>
+            <SearchBar />
+            <div className='container'>
+                <Logs />
+            </div>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
+
+const Logs = () => {
+    const [logs, setLogs] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getLogs();
+        // eslint-disable-next-line
+    }, []);
+
+    const getLogs = async () => {
+        setLoading(true);
+        const res = await fetch('/logs');
+        const data = await res.json();
+
+        setLogs(data);
+        setLoading(false);
+    };
+
+    if (loading) {
+        return <Preloader />;
+    }
+
+    return (
+        <div>
+            <ul className='collection with-header'>
+                <li className='collection-header'>
+                    <h4 className='center'>System Logs</h4>
+                </li>
+                {!loading && logs.length === 0 ? (
+                    <p className='center'>No logs to show...</p>
+                ) : (
+                    logs.map((log, key) => <LogItem key={key} log={log} />)
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export default Logs;
