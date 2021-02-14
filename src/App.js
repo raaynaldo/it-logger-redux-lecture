dfba36cd2d904827aaf3c22f1204b95a56019d95
raaynaldo <raaynaldo@hotmail.com>
2021-02-13 21:56:10 -0500

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
-import './App.css';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import Logs from './components/logs/Logs';
+import './App.css';
 
 const App = () => {
     useEffect(() => {
@@ -13,6 +15,7 @@ const App = () => {
         <>
             <SearchBar />
             <div className='container'>
+                <AddBtn />
                 <Logs />
             </div>
         </>
